349faf679e4bd1b1cbb7edc5c859b8cc0d13d7fa
cormac-dunphy <cormac-d@hotmail.com>
2017-10-18 12:30:08 +0100

    Add serializer class which creates a xml file for users hashmap

---
+++ src/utils/Serializer.java
@@ -0,0 +1,18 @@
+package utils;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+public class Serializer {
+	public void Serialize() throws IOException
+	{
+		XStream xstream = new XStream(new DomDriver());
+		ObjectOutputStream out = xstream.createObjectOutputStream(new FileWriter("users.xml"));
+		out.writeObject(Importer.userMap);
+		out.close();
+	}
+}
